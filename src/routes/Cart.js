0bd6b7838b4bd5838a3d011f36cc554335d21bdf
sojitradrashti <sojitradrashti5@gmail.com>
2024-01-17 07:41:02 -0800

    commit

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
 const Cart = ({ cart, setCart, handleChange }) => {
   const [price, setPrice] = useState(0);
 
@@ -12,8 +11,8 @@ const Cart = ({ cart, setCart, handleChange }) => {
   };
 
   const handleRemove = (id) => {
-   const arr = cart.filter((item) => item.id != id);
-   setCart(arr);
+    const arr = cart.filter((item) => item.id != id);
+    setCart(arr);
   };
 
   useEffect(() => {
@@ -23,41 +22,65 @@ const Cart = ({ cart, setCart, handleChange }) => {
   return (
     <div className="w-[90%] mx-auto mt-[190px]">
       {cart.map((item, index) => (
-        <div key={index} className="flex items-center justify-between mt-[20px] pb-[5px] border border-black">
+        <div
+          key={index}
+          className="flex items-center justify-between mt-[20px] pb-[5px] border border-black"
+        >
           <div className="flex w-[400px] items-center gap-3">
             <img className="w-[100px] h-[90px] p-2" src={item.img} />
             <p className="font-bold">{item.name}</p>
           </div>
           <div>
-            <button onClick={() => handleChange(item, +1)} className="w-[20px] bg-slate-300 border mr-2 border-gray-500">
+            <button
+              onClick={() => handleChange(item, +1)}
+              className="w-[20px] bg-slate-300 border mr-2 border-gray-500"
+            >
               +
             </button>
-            
+
             <button className="w-[20px] bg-slate-300 border mr-2 border-gray-500">
               {item.amount}
             </button>
-            <button onClick={() => handleChange(item, -1)} className="w-[20px] bg-slate-300 border mr-2 border-gray-500">
+            <button
+              onClick={() => handleChange(item, -1)}
+              className="w-[20px] bg-slate-300 border mr-2 border-gray-500"
+            >
               -
             </button>
           </div>
           <div className="flex">
             <span>{item.price}</span>
-            <button onClick={() => handleRemove(item.id)} className="px-2 text-white text-xl bg-red-400  hover:bg-lime-500 rounded-md cursor-pointer">
+            <button
+              onClick={() => handleRemove(item.id)}
+              className="px-2 text-white text-xl bg-red-400  hover:bg-lime-500 rounded-md cursor-pointer"
+            >
               Remove
             </button>
           </div>
         </div>
       ))}
-      <div className="justify-between flex">
-        <span className="font-bold text-green-700 text-3xl">Subtotal
-        </span>
-        <span className="font-bold text-green-700 text-3xl"> ${price}</span>
-      </div>
-      <div className="text-center">
-        <Link to="/Checkout">
-        <button className="p-3 text-white bg-red-500 hover:bg-lime-500 m-5 text-2xl rounded-full">PROCEED TO CHECKOUT</button>
-        </Link>
+      <br />
+
+      <div className="w-[30%] border border-black ml-auto">
+        <div className="border-b-2 border-black ">
+          <h2 className="text-2xl m-3">Cart totals</h2>
+          <hr />
         </div>
+        <div className="flex justify-between border-b-2 p-2">
+          <p>subtotal</p>
+          <p>${price}</p>
+        </div>
+        <div className="flex justify-between border-b-2 p-2">
+          <p>total</p>
+          <p>{price}</p>
+        </div>
+        <Link to="/Checkout">
+          <button className="px-7 p-3 text-white bg-red-500 hover:bg-lime-500 m-5 text-2xl rounded-full">
+            PROCEED TO CHECKOUT
+          </button>
+        </Link>
+      </div>
+      <div className="text-center"></div>
     </div>
   );
 };
--- src/routes/Checkout.js
+++ src/routes/Checkout.js
@@ -1,11 +1,219 @@
-import React from 'react'
+import React from "react";
+import Select from "react-select";
+import { useState, useEffect} from "react";
 
-function Checkout() {
+const options = [
+  { value: "Andhra Pradesh", label: "Andhra Pradesh" },
+  { value: "Arunachal Pradesh", label: "Arunachal Pradesh" },
+  { value: "Assam", label: "Assam" },
+  { value: "Bihar", label: "Bihar" },
+  { value: "Chhatisgarh", label: "Chhatisgarh" },
+  { value: "Goa", label: "Goa" },
+  { value: "Gujarat", label: "Gujarat" },
+  { value: "Haryana", label: "Haryana" },
+  { value: "Himachal Pradesh", label: "Himachal Pradesh" },
+  { value: "Jammu and Kashmir", label: "Jammu and Kashmir" },
+  { value: "Karnataka", label: "Karnataka" },
+  { value: "Kerala", label: "Kerala" },
+  { value: "Ladakh", label: "Ladakh" },
+  { value: "Madhya Pradesh", label: "Madhya Pradesh" },
+  { value: "Maharastra", label: "Maharastra" },
+  { value: "Manipur", label: "Manipur" },
+  { value: "Meghalaya", label: "Meghalaya" },
+  { value: "Mizoram", label: "Mizoram" },
+  { value: "Nagaland", label: "Nagaland" },
+  { value: "Odhisa", label: "Odhisa" },
+  { value: "Punjab", label: "Punjab" },
+  { value: "Rajasthan", label: "Rajasthan" },
+  { value: "Sikkim", label: "Sikkim" },
+  { value: "Tamil Nadu", label: "Tamil Nadu" },
+  { value: "Telangana", label: "Telangana" },
+  { value: "Tripura", label: "Tripura" },
+  { value: "Uttarakhand", label: "Uttarakhand" },
+  { value: "Uttar Pradesh", label: "Uttar Pradesh" },
+  { value: "West Bangal", label: "West Bangal" },
+  {value: "Andaman and Nikobar Islands",label: "Andaman and Nikobar Islands",},
+  { value: "Chandigarh", label: "Chandigarh" },
+  { value: "Dadra And Nagar Haveli", label: "Dadra And Nagar Haveli" },
+  { value: "Daman and Diu", label: "Daman and Diu" },
+  { value: "Delhi", label: "Delhi" },
+  { value: "Lakshadeep", label: "Lakshadeep" },
+  { value: "Pondicherry", label: "Pondicherry" },
+];
+
+const Checkout = (cart,setCart) => {
+  const [price, setPrice] = useState(0);
+
+  const handlePrice = () => {
+    let ans = 0;
+    cart.map((item) => (ans += item.amount * item.price));
+    setPrice(ans);
+  };
+  useEffect(() => {
+    handlePrice();
+  });
   return (
-    <div className='mt-[700px]'>Checkout
-    <hr/>
-    </div>
-  )
-}
+    <div className="mt-[160px] px-20 w-[100%]">
+      <div className=" border border-black items-center ">
+        <div>
+          <h1 className="ml-5 p-4 font-bold text-5xl">Checkout</h1>
+          <hr className="border border-black border-b-2 m-2" />
+          <div>
+            <h2 className="px-3 py-6 text-2xl text-slate-800 font-bold">
+              Billing Details
+            </h2>
+            <hr className="border border-slate-400 border-b-2 m-2" />
+          </div>
+        </div>
 
-export default Checkout
+        <div className="flex">
+          <form>
+            <div className=" m-6">
+              <div className="text-xl flex gap-20 ">
+                <div>
+                  <label for="first name"> First Name </label>
+                  <br />
+                  <input
+                    type="text"
+                    name="First Name"
+                    id="fname"
+                    className="p-2  border-slate-900 border border-dotted"
+                  />
+                  <br />
+                </div>
+
+                <div className="text-xl">
+                  <label for="last name"> Last Name </label>
+                  <br />
+                  <input
+                    type="text"
+                    name="last Name"
+                    className="p-2 border-slate-900 border border-dotted"
+                  />
+                </div>
+              </div>
+
+              <div className="text-xl mt-5 ">
+                <label for="Country / Region ">Country / Region</label>
+
+                <div>
+                  <input
+                    type="text"
+                    name="Country / Region "
+                    value="India"
+                    className="p-2 border-slate-900 border border-dotted w-full"
+                  />
+                </div>
+              </div>
+
+              <div className="text-xl mt-5 ">
+                <label for="Street address">Street address</label>
+
+                <div>
+                  <input
+                    type="text"
+                    name="Street address"
+                    className="p-2 border-slate-900 border border-dotted w-full"
+                  />
+                </div>
+              </div>
+
+              <div className="text-xl mt-5 ">
+                <label for="State">State</label>
+
+                <div>
+                  <Select
+                    options={options}
+                    className="border-slate-900 border border-dotted"
+                  />
+                </div>
+              </div>
+
+              <div className="text-xl mt-5 ">
+                <label for="pin code">PIN Code</label>
+
+                <div>
+                  <input
+                    type="text"
+                    name="pin code"
+                    className="p-2 border-slate-900 border border-dotted w-full"
+                  />
+                </div>
+              </div>
+
+              <div className="text-xl mt-5 ">
+                <label for="phone number">Phone</label>
+
+                <div>
+                  <input
+                    type="text"
+                    name="phone number"
+                    className="p-2 border-slate-900 border border-dotted w-full"
+                  />
+                </div>
+              </div>
+
+              <div className="text-xl mt-5 ">
+                <label for="Email Address">Email Address</label>
+
+                <div>
+                  <input
+                    type="text"
+                    name="email address"
+                    className="p-2 border-slate-900 border border-dotted w-full"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <h1 className="text-2xl font-bold">Additional Information</h1>
+                <hr />
+                <div>
+                  <label
+                    for="Order notes (optional)"
+                    className="text-xl font-bold"
+                  >
+                    Order notes (optional)
+                  </label>
+                  <div>
+                    <textarea
+                      type="text"
+                      rows="3"
+                      placeholder="Notes about your order, e.g. special notes for delivery."
+                      className="p-2 border-slate-900 border border-dotted w-full"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+          <>
+            {/* <div className="border border-black ">
+            <div>
+                 <h1 className="m-6 p-6 text-3xl font-bold">Your order</h1>
+              </div>
+              {cart.map((item,index)=>(
+             <div key={index} className="flex justify-between">
+           
+               <h1>Product</h1>
+               <p>{item.name}</p>
+               <p>{price}</p>
+             
+              ))}
+             
+               
+              
+                {/* <hr className="bg-slate-800"/> */}
+                <div>
+                  <h1>Subtotal</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
